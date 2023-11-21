c464baed14dc5e723258f58ff752359c0e7dee8a
gabee0 <128483742+gabee0@users.noreply.github.com>
2023-11-21 07:51:57 -0300

    hujkyu

--- galeria.js
+++ galeria.js
@@ -3,3 +3,5 @@ const panel = document.querySelectorAll('.panel')
 panels.forEach(panel => {
     panel.addEventListener('click', () => { removeActiveClasses() panel.classList.add('active') })
 })
+
+function removeActiveClasses() { panels.forEach(panel => { panel.classList.remove('active') }) }
